d0106cbc109f2679609da679cf31d0a975b25fec
QuanhBk <quanganhngo2211@gmail.com>
2026-03-12 20:32:22 +0700

    [UPDATE]: restaurant_table - add some view + trigger

--- backend/database/Reservation.sql
+++ backend/database/Reservation.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS reservation (
   session_id VARCHAR(255) NOT NULL, -- TRƯỜNG HỢP ĐẶT BÀN MÀ KHÔNG ĐĂNG NHẬP VÔ HỆ THỐNG
   table_id INT NOT NULL REFERENCES restaurant_table(table_id) ON DELETE CASCADE,
   number_of_guests INT NOT NULL CHECK (number_of_guests > 0)
-  reservation_state RESERVATION_STATUS_TYPE NOT NULL DEFAULT 'CONFIRM'
+  reservation_state RESERVATION_STATUS_TYPE NOT NULL DEFAULT 'CONFIRM',
   
-  reservation_time TIMPESTAMPTZ NOT NULL DEFAULT NOW(),
+  reservation_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
   note VARCHAR(255) -- Khách ghi chú khi đặt bàn 
   restaurant_note VARCHAR(255) REFERENCES restaurant_table(restaurant_note) -- Ghi chú từ phía nhà hàng (nếu có)
@@ -23,5 +23,5 @@ CREATE TABLE IF NOT EXISTS reservation (
 
 
 --TẠO INDEX
-CREATE INDEX idx_reservation_time ON reservation(reservation_time) -- Thống kê đặt bàn theo giờ
+CREATE INDEX idx_reservation_table_time ON reservation(table_id, reservation_time) -- Thống kê đặt bàn theo giờ
 CREATE INDEX idx_reservation_state ON reservation(reservation_state) --Thống kê bàn theo từng trạng thái theo thời gian thực 
--- backend/database/restaurant_table.sql
+++ backend/database/restaurant_table.sql
@@ -18,6 +18,87 @@ CREATE TABLE IF NOT EXISTS restaurant_table (
 
 
 -- TẠO INDEX
+CREATE INDEX dx_table_position ON restaurant_table (position_x, position_y)
 CREATE INDEX idx_table_status ON restaurant_table(table_status)
 CREATE INDEX idx_table_capacity ON restaurant_table(capacity)
 
+
+
+-- VIEW: AVAILABLE AT REALTIME : Bàn đang trống theo thời gian thực (Tại chính thời điểm truy vấn)
+-- TH1: Bàn ở trạng thái 'available' theo thời gian đang xét
+-- TH2: Bàn có Reservation time >= Now() (Nhưng cần hiển thị khoảng thời gian từ now -> reservation)
+
+CREATE VIEW available_table_now AS
+
+WITH next_reservation AS (
+    SELECT 
+        table_id,
+        MIN(reservation_time) AS next_reservation_time
+    FROM reservation
+    WHERE reservation_state = 'CONFIRM'
+      AND reservation_time > NOW()
+      AND reservation_time >= CURRENT_DATE
+      AND reservation_time < CURRENT_DATE + INTERVAL '1 day'
+    GROUP BY table_id
+)
+
+SELECT 
+    t.table_id,
+    t.capacity,
+    nr.next_reservation_time,
+
+    EXTRACT(EPOCH FROM (
+        nr.next_reservation_time - NOW()
+    )) / 60 AS minutes_until_next -- Tính khoảng thời gian từ now -> nearest_reservation_time
+
+FROM restaurant_table t
+
+LEFT JOIN next_reservation nr
+ON t.table_id = nr.table_id
+
+WHERE t.table_status != 'OCCUPIED';
+
+-- VIEW: RESERVATION BOOKING IN DAY
+CREATE VIEW reservation_in_day AS 
+    SELECT
+        t.table_id,
+        t.capacity,
+        r.reservation_id,
+        r.reservation_time,
+        r.reservation_state 
+    FROM restaurant_table t 
+    LEFT JOIN reservation r 
+    ON t.table_id = r.table_id
+    AND r.reservation_time >= CURRENT_DATE
+    AND r.reservation_time < CURRENT_DATE + INTERVAL '1 day';
+         
+
+
+-- TRIGGER: CẬP NHẬT TRẠNG THÁI BÀN THEO TRẠNG THÁI ĐẶT BÀN
+CREATE OR REPLACE FUNCTION update_table_status()
+RETURNS TRIGGER AS $$
+BEGIN
+
+    IF NEW.reservation_state = 'ON_SERVING' THEN
+        UPDATE restaurant_table
+        SET table_status = 'OCCUPIED'
+        WHERE table_id = NEW.table_id;
+
+    ELSIF NEW.reservation_state IN ('CANCELED', 'COMPLETED') THEN
+        UPDATE restaurant_table
+        SET table_status = 'AVAILABLE'
+        WHERE table_id = NEW.table_id;
+
+    END IF;
+
+    RETURN NEW;
+
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGER KÍCH HOẠT TRIGGER KHI CÓ SỰ THAY ĐỔI TRẠNG THÁI ĐẶT BÀN
+CREATE TRIGGER trg_update_table_status
+AFTER UPDATE ON reservation
+FOR EACH ROW
+WHEN (OLD.reservation_state IS DISTINCT FROM NEW.reservation_state)
+EXECUTE FUNCTION update_table_status();
